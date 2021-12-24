a8147fec68d52d9f576207aecd3e5ea2da1182fe
Weiran Cheng <chengwr1128@gmail.com>
2021-12-23 17:24:03 -0800

    Draft the framework for product info

---
+++ client/src/components/productdetails/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+// import '../../../../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+function Navbar() {
+
+  return (
+    <nav className="navbar navbar-inverse navbar-fixed-top">
+      <div className="container">
+        <div className="navbar-header">
+          <a className="navbar-brand" href="#">Logo</a>
+        </div>
+        <div id="navbar" className="navbar-collapse collapse">
+          <form className="navbar-form navbar-right">
+            <div className="form-group">
+              <input type="text" className="form-control" />
+            </div>
+            <button type="submit" className="btn btn-light">Search</button>
+          </form>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
--- client/src/components/productdetails/ProductDetails.jsx
+++ client/src/components/productdetails/ProductDetails.jsx
@@ -1,15 +1,85 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import API_KEY from '../../config/config.js';
+import Navbar from './Navbar.jsx';
+import ProductInfo from './ProductInfo.jsx';
 
-class ProductDetails extends React.Component {
-  constructor(props){
-    super(props);
+function ProductDetails(props) {
+
+  const [info, setInfo] = useState({
+    id: 0,
+    campus: '',
+    name: '',
+    slogan: '',
+    description: '',
+    category: '',
+    default_price: '',
+    created_at: '',
+    updated_at: '',
+    features: []
+  });
+
+  const [style, setStyle] = useState({
+    product_id: 0,
+    results: []
+  });
+
+  const [cart, setCart] = useState([]);
+
+  useEffect(() => {
+    getInfo();
+    getStyle();
+    getCartInfo();
+  }, []);
+
+  const getInfo = () => {
+    axios.get('https://app-hrsei-api.herokuapp.com/api/fec2/hr-lax/products/42366', {
+      headers: {
+        'Authorization': `${API_KEY}`
+      }
+    })
+    .then(response => {
+      setInfo(response.data);
+    })
+    .catch(err => {
+      console.error(err);
+    })
   }
 
-  render() {
-    return (
-      <div></div>
-    )
+  const getStyle = () => {
+    axios.get('https://app-hrsei-api.herokuapp.com/api/fec2/hr-lax/products/42366/styles', {
+      headers: {
+        'Authorization': `${API_KEY}`
+      }
+    })
+    .then(response => {
+      setStyle(response.data);
+    })
+    .catch(err => {
+      console.error(err);
+    })
   }
+
+  const getCartInfo = () => {
+    axios.get('https://app-hrsei-api.herokuapp.com/api/fec2/hr-lax/cart', {
+      headers: {
+        'Authorization': `${API_KEY}`
+      }
+    })
+    .then(response => {
+      setCart(response.data);
+    })
+    .catch(err => {
+      console.error(err);
+    })
+  }
+
+  return (
+    <div>
+      <Navbar />
+      <ProductInfo info={info} style={style.results} />
+    </div>
+  )
 }
 
 export default ProductDetails;
