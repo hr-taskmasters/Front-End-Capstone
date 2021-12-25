ad6536a874475c815fa253c1a9b0a53ab1a37a65
davidsung <dks99455@gmail.com>
2021-12-24 16:04:26 -0800

    ListCarousel file created

---
+++ client/src/components/relateditems/ListCarousel.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../../../dist/style.css';
+import ProductCard from './ProductCard.jsx';
+
+const ListCarousel = (props) => {
+
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default ListCarousel;
