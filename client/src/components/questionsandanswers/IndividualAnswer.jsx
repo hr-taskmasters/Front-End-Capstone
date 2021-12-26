a9384591fbb2e4182e3c610f80d6db05d63229de
“Cynthia <xin2604@gmail.com>
2021-12-26 10:12:00 -0800

    update an answer to show it has been reported

--- client/src/components/questionsandanswers/IndividualAnswer.jsx
+++ client/src/components/questionsandanswers/IndividualAnswer.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect} from 'react';
 import moment from 'moment';
 import { Stack, Image, ListGroup } from 'react-bootstrap';
+import axios from 'axios';
+import API_KEY from '../../config/config.js';
 
 const IndividualAnswer = (props) => {
   const [a_helpful_count, setACount] = useState(() => {return props.answer.helpfulness})
@@ -8,25 +10,40 @@ const IndividualAnswer = (props) => {
     setACount(a_helpful_count + 1)
   }
   const photos = props.answer.photos;
+  const answer_id = props.answer.answer_id;
+
+  const [reported, setReported] = useState(false);
+  const markReport = () => {
+    axios.put(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-lax/qa/answers/${answer_id}/report`,
+      { reported: true } ,{
+      headers: { 'Authorization': `${API_KEY}` }
+      })
+        .then((response) => {
+          setReported(true);
+        })
+        .catch((err) => console.error(err));
+  }
+
   return (
     <div>
-      <Stack gap={2} className="col-md-5 mx-auto">
-        {/* <strong>A:</strong> */}
-        <div>
-          {props.answer.body}
-        </div>
+      <Stack gap={4}>
+        <div>{props.answer.body}</div>
         <Stack direction="horizontal" gap={6}>
           <div>
             by {props.answer.answerer_name}, {moment(props.answer.date).format('MMMM Do YYYY')}
           </div>
-          <div className="vr"/>
+          <div className="vr ms-auto"/>
           <div className="ms-auto">
             <label> Helpful? </label>
             <u onClick={handleHelpfulClick}>Yes</u>
             <span>({a_helpful_count})</span>
           </div>
-          <div className="vr"/>
-          <u className="ms-auto">Report</u>
+          <div className="vr ms-auto"/>
+          {!reported ? (
+            <u className="ms-auto" onClick={() => markReport()}>Report</u>
+          ) : (
+            <u className="ms-auto">Reported</u>
+          )}
         </Stack>
         {photos.length > 0 &&
           <div>
@@ -42,37 +59,6 @@ const IndividualAnswer = (props) => {
       </Stack>
     </div>
   )
-  // return (
-  //   <ListGroup.Item className="Q-answer-container">
-  //     <div className="Q-answer-body">
-  //     <label className="Q-answer">
-  //       <strong>A:</strong>
-  //       {props.answer.body}
-  //     </label>
-  //     <label className="Q-answererAndDate">
-  //       by {props.answer.answerer_name}, {moment(props.answer.date).format('MMMM Do YYYY')}
-  //     </label>
-  //     <span className="Q-answer-helpful">
-  //       <label> Helpful? </label>
-  //       <u onClick={handleHelpfulClick}>Yes</u>
-  //       <span>({a_helpful_count})</span>
-  //     </span>
-  //     <span> | </span>
-  //     <u>Report</u>
-  //   </div>
-  //   {photos.length > 0 &&
-  //     <div>
-  //       <label>Yes, as you can see in these photos.</label>
-  //       <div>
-  //         {photos.map((photo) =>
-  //         <Image src={photo.url} thumbnail />
-  //         )}
-  //       </div>
-  //     </div>
-  //   }
-  //   <br></br>
-  //   </ListGroup.Item>
-  // )
 }
 
 export default IndividualAnswer;
