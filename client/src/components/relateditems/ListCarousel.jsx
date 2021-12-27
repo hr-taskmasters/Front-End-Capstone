2e118e0bd1a36b14711c2c97c33ab1adaa345a71
davidsung <dks99455@gmail.com>
2021-12-27 11:41:36 -0800

    initial carousel prototype

--- client/src/components/relateditems/ListCarousel.jsx
+++ client/src/components/relateditems/ListCarousel.jsx
@@ -1,14 +1,73 @@
 import React from 'react';
-import '../../../dist/style.css';
 import ProductCard from './ProductCard.jsx';
 
-const ListCarousel = (props) => {
+// sample data
+import getProducts from './data/sampleDataProducts.js';
 
-  return (
-    <div>
+class ListCarousel extends React.Component {
+  constructor(props){
+    super(props);
 
-    </div>
-  )
+    this.state = {
+      grid: []
+    }
+
+    this.populateCarousel = this.populateCarousel.bind(this);
+    this.scrollLeft = this.scrollLeft.bind(this);
+    this.scrollRight = this. scrollRight.bind(this);
+  }
+  scrollPos = 0;
+  scrollDistance = 400;
+
+  componentDidMount() {
+    this.populateCarousel();
+  }
+
+  populateCarousel () {
+    let grid = [];
+    getProducts.map((product, index) => {
+      grid.push(<div className="carouselItem" key={index}>{product.name}</div>)
+    })
+    this.setState({
+      grid: grid
+    })
+  }
+
+  scrollLeft () {
+    document.querySelector('.carouselBox').scrollTo({
+      top: 0,
+      left: (this.scrollPos -= this.scrollDistance),
+      behavior: "smooth"
+    })
+
+    if(this.scrollPos < 0){
+      this.scrollPos = 0;
+    }
+  }
+
+  scrollRight () {
+    if(this.scrollPos <= document.querySelector('.carouselBox').scrollWidth - document.querySelector('.carouselBox').clientWidth) {
+      document.querySelector('.carouselBox').scrollTo({
+        top: 0,
+        left: (this.scrollPos += this.scrollDistance),
+        behavior: "smooth"
+      })
+    }
+  }
+
+  render() {
+    return (
+      <div className='carousel'>
+        <div className='carouselContainer'>
+          <div className="carouselBox">
+          {!Array.isArray(this.props.items) ? 'Loading...' : this.state.grid}
+          </div>
+          <div className="moveLeft slideButton" onClick={this.scrollLeft}>{'<'}</div>
+          <div className="moveRight slideButton" onClick={this.scrollRight}>{'>'}</div>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default ListCarousel;
--- client/src/components/relateditems/RelatedItems.jsx
+++ client/src/components/relateditems/RelatedItems.jsx
@@ -1,21 +1,27 @@
 import React from 'react';
 import ProductCard from './ProductCard.jsx';
 import {Carousel} from 'react-bootstrap';
+import ListCarousel from './ListCarousel.jsx';
 
 //sample data
 import initialState from './data/sampleDataProduct.js';
+import getProducts from './data/sampleDataProducts.js';
 
 const RelatedItems = (props) => {
 
   return (
-    <Carousel variant="dark">
-      <Carousel.Item>
-        <ProductCard product={initialState.products[0]} ratings={props.ratings}/>
-      </Carousel.Item>
-      <Carousel.Item>
-        <ProductCard product={initialState.products[0]} ratings={props.ratings}/>
-      </Carousel.Item>
-    </Carousel>
+    <div>
+      <ListCarousel items={getProducts} />
+    </div>
+
+    // <Carousel variant="dark">
+    //   <Carousel.Item>
+    //     <ProductCard product={initialState.products[0]} ratings={props.ratings}/>
+    //   </Carousel.Item>
+    //   <Carousel.Item>
+    //     <ProductCard product={initialState.products[0]} ratings={props.ratings}/>
+    //   </Carousel.Item>
+    // </Carousel>
   )
 }
 
