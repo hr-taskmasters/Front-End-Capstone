0b029575c8233e1f6a8ba90a22c195da5670ae06
Weiran Cheng <chengwr1128@gmail.com>
2021-12-27 10:22:56 -0800

    handle click for each thumbnail image

--- client/src/components/productdetails/Image.jsx
+++ client/src/components/productdetails/Image.jsx
@@ -1,19 +1,36 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Carousel from 'react-bootstrap/Carousel';
 
 function Image(props) {
+
+  const [selectImg, setSelectImg] = useState(0);
+  const [click, setClick] = useState(false);
+
+  const handleClick = (index) => {
+    setSelectImg(index);
+    setClick(true);
+  };
 
   return (
     <div className='row'>
       <div className='col-md-2'>
         {props.style.length ?
           props.style[props.styleNum].photos.map((img, index) => (
-          <img className='p_s_img' key={index} src={img.thumbnail_url}></img>)) : null}
+          <img
+            className='p_s_img'
+            key={index}
+            src={img.thumbnail_url}
+            onClick={() => handleClick(index)}
+          ></img>)) : null}
       </div>
       <div className='col-md-10'>
         {props.style.length ?
         <Carousel variant='dark'>
-          {props.style[props.styleNum].photos.map((img, index) => (
+          {click ?
+          <img className='w-100 p_pic' src={props.style[props.styleNum].photos[selectImg].url}>
+          </img>
+          :
+          props.style[props.styleNum].photos.map((img, index) => (
             <Carousel.Item key={index} interval={5000}>
               <img
                 className='d-block w-100 p_pic'
