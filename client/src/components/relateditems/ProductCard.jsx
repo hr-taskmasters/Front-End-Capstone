414893abfc8b3bfe188c37137a1287adf91b4668
davidsung <dks99455@gmail.com>
2021-12-27 19:32:05 -0800

    partial axios response implemented

--- client/src/components/relateditems/ProductCard.jsx
+++ client/src/components/relateditems/ProductCard.jsx
@@ -1,35 +1,59 @@
 import React, { useState, useEffect } from 'react';
 import ActionButton from './ActionButton.jsx';
+import axios from 'axios';
+import API_KEY from '../../config/config.js';
 
 //sample data. Need to make /products/:id/styles query to retrieve
 import styleCall from './data/sampleDataProductCard.js';
 
-const ProductCard = (props) => {
+class ProductCard extends React.Component {
+  constructor(props) {
+    super(props);
 
-  let category = props.product.category.toUpperCase();
-  let imageUrl = styleCall.results[0].photos[0].thumbnail_url;
-  let expandedProductName = props.product.name + ' - ' + styleCall.results[0].name;
-  let price = styleCall.results[0].original_price;
-  let totalRatings = (Number(props.ratings[1]) + Number(props.ratings[2]) + Number(props.ratings[3]) + Number(props.ratings[4]) + Number(props.ratings[5]))
-  let rating = (Number(props.ratings[1]) * 1 + Number(props.ratings[2]) * 2 + Number(props.ratings[3]) * 3 + Number(props.ratings[4]) * 4 + Number(props.ratings[5]) * 5)/ totalRatings;
+    this.state = {
+      category: this.props.product.category.toUpperCase(),
+      imageUrl: '',
+      // styleCall.results[0].photos[0].thumbnail_url;
+      expandedProductName: this.props.product.name + ' - ' + styleCall.results[0].name,
+      price: this.props.product.default_price,
+      rating: (Number(this.props.ratings[1]) * 1 + Number(this.props.ratings[2]) * 2 + Number(this.props.ratings[3]) * 3 + Number(this.props.ratings[4]) * 4 + Number(this.props.ratings[5]) * 5)/ (Number(this.props.ratings[1]) + Number(this.props.ratings[2]) + Number(this.props.ratings[3]) + Number(this.props.ratings[4]) + Number(this.props.ratings[5]))
+    }
+  }
 
-  return (
-    <div>
-      <div className="imageContainer">
-        <img src={imageUrl}/>
-      </div>
-      <div className="buttonContainer">
-        <ActionButton />
-      </div>
+  componentDidMount(){
+    axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-lax/products/${this.props.product.id}/styles`, {
+    headers: {
+      'Authorization': `${API_KEY}`
+      }
+    })
+    .then((product) => (
+      this.setState({
+        imageUrl: product.data.results[0].photos[0].thumbnail_url
+      })
+    ))
+    .catch((err) => {
+      console.log(err)
+    })
+  }
+
+  render() {
+    return (
       <div>
-        <div>{category}</div>
-        <div>{expandedProductName}</div>
-        <div>${price}</div>
-        <div>{(props.ratings[1] === "" && props.ratings[2] === "" && props.ratings[3] === "" && props.ratings[4] === "" && props.ratings[5] === "")
-               ? 'Loading...' : rating}</div>
+        <div className="imageContainer">
+          <img src={this.state.imageUrl}/>
+        </div>
+        <div className="buttonContainer">
+          <ActionButton />
+        </div>
+        <div>
+          <div>{this.state.category}</div>
+          <div>{this.state.expandedProductName}</div>
+          <div>${this.state.price}</div>
+          <div>{this.state.rating}</div>
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default ProductCard;
