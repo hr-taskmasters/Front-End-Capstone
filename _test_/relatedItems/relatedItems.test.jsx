fece1e00539953e917bf4d98f06766d272225f72
davidsung <dks99455@gmail.com>
2021-12-29 11:09:31 -0800

    jesttest

--- _test_/relatedItems/relatedItems.test.jsx
+++ _test_/relatedItems/relatedItems.test.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Enzyme, { shallow, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import RelatedItems from '../client/components/relateditems/RelatedItems.jsx';
+import RelatedItems from '../../client/src/components/relateditems/RelatedItems.jsx';
 
 Enzyme.configure({ adapter: new Adapter() });
 
