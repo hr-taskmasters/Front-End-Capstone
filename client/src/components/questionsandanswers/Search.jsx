563a474718130fbdcf431ae92432a3c1f64d113c
“Cynthia <xin2604@gmail.com>
2021-12-29 09:17:09 -0800

    pull main

--- client/src/components/questionsandanswers/Search.jsx
+++ client/src/components/questionsandanswers/Search.jsx
@@ -1,18 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Form, InputGroup } from 'react-bootstrap';
 
 const Search = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+  const getSearchTerm = (e) => {
+    if(e.target.value.length > 3) {
+      console.log(e.target.value);
+    }
+  }
   return (
     <Form className='q_searchbar' fluid>
       <InputGroup>
+      <i class="fas fa-search"></i>
         <Form.Control
           type='search'
           placeholder='HAVING A QUESTION? SEARCH FOR ANSWERS...'
+          onChange={getSearchTerm}
+          // onKeyUp={getSearchTerm}
         >
         </Form.Control>
         <InputGroup.Text><i class="fas fa-search"></i></InputGroup.Text>
       </InputGroup>
-
     </Form>
   )
 }
