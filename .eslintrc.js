1a40c4a5d45d8add544102dcb5ddcb5616f120de
davidsung <dks99455@gmail.com>
2021-12-29 18:16:59 -0800

    partial implementation of eslint for react

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,11 @@
 module.exports = {
-  extends: 'eslint:recommended',
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended'
+  ],
   rules: {
     // Rules here will override the 'hackreactor' configuration
     // http://eslint.org/docs/rules/
+    "parser": "babel-eslint"
   }
 };
