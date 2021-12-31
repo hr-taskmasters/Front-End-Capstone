ef213e9ab955c8dedffc0325fd6f05f807181505
davidsung <dks99455@gmail.com>
2021-12-30 19:48:04 -0800

    partial implementation of modal

--- client/src/components/relateditems/ActionButton.jsx
+++ client/src/components/relateditems/ActionButton.jsx
@@ -1,12 +1,62 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
+import {Container, Row, Col} from'react-bootstrap';
 
 const ActionButton = (props) => {
 
   const icon = props.icon === 'star' ? <i className='far fa-star'></i> : <i className='far fa-xmark'></i>;
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
 
   return (
-    <span className='actionButton'>{icon}</span>
+    <>
+      <span className='actionButton' onClick={(e) => {
+        handleShow();
+        e.stopPropagation();
+      }}>{icon}</span>
+      <Modal show={show} dialogClassName="compareModal" onHide={(e) => {
+        handleClose()
+        e.stopPropagation();
+      }}>
+        <Modal.Header closeButton>
+          <Modal.Title>Comparing</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Container fluid>
+            <Row>
+              <Col>Product Placeholder 1</Col>
+              <Col></Col>
+              <Col>Product Placeholder 2</Col>
+            </Row>
+          </Container>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={(e) => {
+            handleClose()
+            e.stopPropagation();
+          }}>Close</Button>
+        </Modal.Footer>
+      </Modal>
+    </>
   );
 };
 
 export default ActionButton;
+
+const styles = {
+  cardImage: {
+    maxWidth: '100%',
+    height: '300px',
+    overflow: 'hidden'
+  },
+  cardText: {
+    whiteSpace: 'normal',
+    textAlign: 'left'
+  },
+  noMargin: {
+    marginBottom: 0
+  }
+}
