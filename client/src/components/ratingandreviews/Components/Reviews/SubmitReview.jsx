331c073f1d7b8a3a68066b03930c1f4cadd9b778
Ben Pintel <bpintel@gmail.com>
2021-12-31 22:11:43 -0800

    submit reviews almost done - stil getting 422 error

--- client/src/components/ratingandreviews/Components/Reviews/SubmitReview.jsx
+++ client/src/components/ratingandreviews/Components/Reviews/SubmitReview.jsx
@@ -1,19 +1,32 @@
 import React, { useState, useEffect} from 'react';
 import axios from 'axios';
-import API_KEY from '../../../../config/config.js'
-import { 
-    Button, Stack, Row, Form, Modal, 
-    Accordion, FloatingLabel, ButtonGroup, ToggleButton, ToggleButtonGroup
-} from 'react-bootstrap';
+import API_KEY from '../../../../config/config.js';
+import Radios from '../../radioData/radioData.js';
+import { Rating } from 'react-simple-star-rating';
+
+import { Button, Stack, Form, Modal, Accordion, FloatingLabel, ButtonGroup, ToggleButton } from 'react-bootstrap';
 
 
 function SubmitReview (props) {
     const [showModal, setShowModal] = useState(false);
-    const [applicableChars, setApplicableChars] = useState([]);
-    //radio buttons
-    const [recommended, setRecommended] = useState('');
+    const [appChars, setAppChars] = useState([]);
+    const [factors, setFactors] = useState(null);
+    //star rating
+    const [stars, setStars] = useState(0);
+    //radio buttons state
+    const [recommended, setRecommended] = useState(null);
     const [size, setSize] = useState(null);
     const [width, setWidth] = useState(null);
+    const [comfort, setComfort] = useState(null);
+    const [quality, setQuality] = useState(null);
+    const [length, setLength] = useState(null);
+    const [fit, setFit] = useState(null);
+    //text inputs state
+    const [summary, setSummary] = useState('');
+    const [body, setBody] = useState('');
+    const [nickname, setNickname] = useState('');
+    const [email, setEmail] = useState('');
+    
     
     const handleShow = () => setShowModal(true);
     const handleClose = () => setShowModal(false);
@@ -21,57 +34,118 @@ function SubmitReview (props) {
     const findAppChars = () => {
         if(props.metaData.characteristics !== undefined){
         const charArr = Object.keys(props.metaData.characteristics);
-            setApplicableChars(charArr);
+            setAppChars(charArr);
         }
     }
     useEffect(()=> {
         findAppChars()
     }, [props.metaData.characteristics]);
 
-    //radio arrays
-    const recommendedRadios =[
-        { name: 'Yes', value: true},
-        { name: 'No', value: false}
-    ];
+    const handleRating = (rate) => {
+        setStars(rate);
+    }
 
-    const sizeRadios = [
-        { name: 'A size too small', value: 1 },
-        { name: '1/2 a size too small', value: 2 },
-        { name: 'Perfect', value: 3 },
-        { name: '1/2 a size too big', value: 4 },
-        { name: 'A size too big', value: 5 }
-    ];
+    // useEffect(() => {
+    //     createAndParseFactorsObj()
+    // },[])
 
-    const widthRadios = [
-        { name: 'Too narrow', value: 1 },
-        { name: 'Slightly narrow', value: 2 },
-        { name: 'Perfect', value: 3 },
-        { name: 'Slightly wide', value: 4 },
-        { name: 'Too wide', value: 5 }
-    ];
+    // const createAndParseFactorsObj = () => {
+    //     if(props.metaData.characteristics !== undefined){
+    //         const sizeKey = props.metaData.characteristics.Size.id;
+    //         const widthKey = props.metaData.characteristics.Width.id;
+    //         const comfortKey = props.metaData.characteristics.Comfort.id;
+    //         const qualityKey = props.metaData.characteristics.Quality.id;
+    //         const lengthKey = props.metaData.characteristics.Length.id;
+    //         const fitKey = props.metaData.characteristics.Fit.id;
+    //         const factorsObj = {
+    //             [sizeKey]: size,
+    //             [widthKey]: width,
+    //             [comfortKey]: comfort,
+    //             [qualityKey]: quality,
+    //             [lengthKey]: length,
+    //             [fitKey]: fit
+    //         }
+    //         for(var key in factorsObj){
+    //             if(factorsObj[key] === null){
+    //                 delete factorsObj[key]
+    //             }
+    //         }
+    //         setFactors(factorsObj)
+    //     }
+    // }
 
- 
+    const postReview = (e) => {
+        e.preventDefault();
+        const factors = {
+            142034: Number(comfort),
+            142032: Number(fit),
+            142033: Number(length),
+            142035: Number(quality)
+        }
+        console.log('PARAMS:',{
+            product_id: props.product.id,
+            rating: (stars / 20),
+            summary: summary,
+            body: body,
+            recommended: Boolean(recommended),
+            name: nickname,
+            email: email,
+            characteristics: factors
+        })
+        // console.log(factors)
+        if ( recommended 
+            && body.length > 50 
+            && nickname.length > 1 
+            && email.length > 1 
+            && email.includes('@') 
+            && factors) {
+                console.log(body.length>50, nickname.length > 1, email.length > 1 && email.includes('@'),factors)
+                // /?product_id=${props.product.id}
+        axios.post(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-lax/reviews/?product_id=${props.product.id}`, {
+            product_id: props.product.id,
+            rating: (stars / 20),
+            summary: summary,
+            body: body,
+            recommended: Boolean(recommended),
+            name: nickname,
+            email: email,
+            photos: ['https://unsplash.com/s/photos/domestic-cat'],
+            characteristics: factors
+        }, { headers: { 'Authorization': `${API_KEY}` } })
+        .then(res =>{
+            alert('Your review was submitted.')
+            handleClose();
+        })
+        .catch(err => console.log(err))
+        } else {
+            alert('All fields marked with a * must be complete.')
+        };
 
+    };
 
-
+    
     return (
       <div>
         <Button variant="outline-secondary" onClick={handleShow}>Submit Review</Button>
-        <Modal show={showModal} onHide={handleClose} backdrop="static" dialogClassName="modal-90w">
+        <Modal show={showModal} onHide={handleClose} backdrop="static" size="lg" dialogClassName="modal-90w">
             <Modal.Header closeButton>
                 <Modal.Title>My Review of "{props.product.name}"</Modal.Title>
             </Modal.Header>
             <Modal.Body>
                 <Stack gap={3}>
+                <b>Rating*</b>
+                <Rating onClick={handleRating} ratingValue={stars}/>
+                
 
-
-
+                    {/* RADIOS */}
                 <b>Do you recommend this product?*</b>
                 <ButtonGroup className="mb-2">
-                  {recommendedRadios.map((radio, i) => (
+                  {Radios.recommended.map((radio, i) => (
                     <ToggleButton
+                        size="sm"
                         key={i}
-                        id={`radio-${i}`}
+                        // active={true}
+                        id={`radioRec-${i}`}
                         type="radio"
                         variant={i % 2 === 0  ? 'outline-primary' : 'outline-secondary'}
                         name="radio"
@@ -83,12 +157,14 @@ function SubmitReview (props) {
                     </ToggleButton>
                   ))}
                 </ButtonGroup>
-                    //RADIO FACTORS
+                {appChars.indexOf('Size') > -1 ?
+                <>
+                <b>Size*</b>
                 <ButtonGroup className="mb-2">
-                  {sizeRadios.map((radio, i) => (
+                  {Radios.size.map((radio, i) => (
                     <ToggleButton
                         key={i}
-                        id={`radio-${i}`}
+                        id={`radioSize-${i}`}
                         type="radio"
                         variant="outline-primary"
                         name="radio"
@@ -100,43 +176,136 @@ function SubmitReview (props) {
                     </ToggleButton>
                   ))}
                 </ButtonGroup>
-
-
-
-
-
-
-
-
-
-
-
-
-
+                </>
+                : <></>}
+                {appChars.indexOf('Width') > -1 ?
+                <>
+                <b>Width*</b>
+                <ButtonGroup className="mb-2">
+                  {Radios.width.map((radio, i) => (
+                    <ToggleButton
+                        key={i}
+                        id={`radioWidth-${i}`}
+                        type="radio"
+                        variant="outline-primary"
+                        name="radio"
+                        value={radio.value}
+                        checked={width === radio.value}
+                        onChange={(e) => setWidth(e.currentTarget.value)}
+                        >
+                        {radio.name}
+                    </ToggleButton>
+                  ))}
+                </ButtonGroup>
+                </>
+                : <></>}
+                {appChars.indexOf('Comfort') > -1 ?
+                <>
+                <b>Comfort*</b>
+                <ButtonGroup className="mb-2">
+                  {Radios.comfort.map((radio, i) => (
+                    <ToggleButton
+                        key={i}
+                        id={`radioComfort-${i}`}
+                        type="radio"
+                        variant="outline-primary"
+                        name="radio"
+                        value={radio.value}
+                        checked={comfort === radio.value}
+                        onChange={(e) => setComfort(e.currentTarget.value)}
+                        >
+                        {radio.name}
+                    </ToggleButton>
+                  ))}
+                </ButtonGroup>
+                </>
+                : <></>}
+                {appChars.indexOf('Quality') > -1 ?
+                <>
+                <b>Quality*</b>
+                <ButtonGroup className="mb-2">
+                  {Radios.width.map((radio, i) => (
+                    <ToggleButton
+                        key={i}
+                        id={`radioQuality-${i}`}
+                        type="radio"
+                        variant="outline-primary"
+                        name="radio"
+                        value={radio.value}
+                        checked={quality === radio.value}
+                        onChange={(e) => setQuality(e.currentTarget.value)}
+                        >
+                        {radio.name}
+                    </ToggleButton>
+                  ))}
+                </ButtonGroup>
+                </>
+                : <></>}
+                {appChars.indexOf('Quality') > -1 ?
+                <>
+                <b>Length*</b>
+                <ButtonGroup className="mb-2">
+                  {Radios.width.map((radio, i) => (
+                    <ToggleButton
+                        key={i}
+                        id={`radioLength-${i}`}
+                        type="radio"
+                        variant="outline-primary"
+                        name="radio"
+                        value={radio.value}
+                        checked={length === radio.value}
+                        onChange={(e) => setLength(e.currentTarget.value)}
+                        >
+                        {radio.name}
+                    </ToggleButton>
+                  ))}
+                </ButtonGroup>
+                </>
+                : <></>}
+                {appChars.indexOf('Quality') > -1 ?
+                <>
+                <b>Fit*</b>
+                <ButtonGroup className="mb-2">
+                  {Radios.width.map((radio, i) => (
+                    <ToggleButton
+                        key={i}
+                        id={`radioFit-${i}`}
+                        type="radio"
+                        variant="outline-primary"
+                        name="radioFit"
+                        value={radio.value}
+                        checked={fit === radio.value}
+                        onChange={(e) => setFit(e.currentTarget.value)}
+                        >
+                        {radio.name}
+                    </ToggleButton>
+                  ))}
+                </ButtonGroup>
+                </>
+                : <></>}
                 <b>Summary</b>
                 <FloatingLabel controlId="floatingInput" label='Example: "Best purchase ever!"' className="mb-3">
                     <Form.Control 
                     type="text" 
+                    as="textarea"
                     placeholder='Example: "Best purchase ever!"' 
                     maxLength="60"
-                    // onChange={}
+                    onChange={(e) => setSummary(e.target.value)}
                     />
                 </FloatingLabel>
-
                 <b>Review body*</b>
                 <FloatingLabel controlId="floatingPassword" label='“Why did you like the product or not?”' className="mb-3">
                     <Form.Control 
-                    className="submit-body"
                     as="textarea"
+                    style={{ height: '260px' }}
                     type="text" 
                     placeholder='“Why did you like the product or not?”' 
                     minLength="50"
                     maxLength="1000"
-                    // onChange={}
+                    onChange={(e) => setBody(e.target.value)}
                     />
                 </FloatingLabel>
-
-                <b>Add Images*</b>
+                <b>Add Images</b>
                 <Accordion>
                 <Accordion.Item eventKey="0">
                     <Accordion.Header>Upload Images</Accordion.Header>
@@ -150,39 +319,35 @@ function SubmitReview (props) {
                     </Accordion.Body>
                 </Accordion.Item>
                 </Accordion>
-
-
                 <b>Nickname*</b>
                 <FloatingLabel controlId="floatingInput" label='Example: "jackson11!”' className="mb-3">
                     <Form.Control 
                     type="text" 
                     placeholder='Example: "jackson11!”' 
                     maxLength="60"
-                    // onChange={}
+                    onChange={(e) => setNickname(e.target.value)}
                     />
                     <Form.Text muted>
                     For privacy reasons, do not use your full name or email address.
                     </Form.Text>
                 </FloatingLabel>
-
                 <b>Email*</b>
                 <FloatingLabel controlId="floatingInput" label='Example: "jackson11@email.com”' className="mb-3">
                     <Form.Control 
                     type="email" 
                     placeholder='Example: "jackson11@email.com”' 
                     maxLength="60"
-                    // onChange={}
+                    onChange={(e) => setEmail(e.target.value)}
                     />
                     <Form.Text muted>
                     For authentication reasons, you will not be emailed.
                     </Form.Text>
                 </FloatingLabel>
-            
             </Stack>
             </Modal.Body>
             <Modal.Footer>
             <Button variant="outline-secondary" onClick={handleClose}>Cancel</Button>
-            <Button variant="outline-primary">Submit</Button>
+            <Button variant="outline-primary" onClick={postReview}>Submit</Button>
             </Modal.Footer>
         </Modal>
       </div>
@@ -218,3 +383,30 @@ export default SubmitReview;
     <Button variant="primary">Understood</Button>
     </Modal.Footer>
 </Modal> */}
+
+
+
+//radio arrays
+// const recommendedRadios =[
+//     { name: 'Yes', value: true},
+//     { name: 'No', value: false}
+// ];
+
+// const sizeRadios = [
+//     { name: 'A size too small', value: 1 },
+//     { name: '1/2 a size too small', value: 2 },
+//     { name: 'Perfect', value: 3 },
+//     { name: '1/2 a size too big', value: 4 },
+//     { name: 'A size too big', value: 5 }
+// ];
+
+// const widthRadios = [
+//     { name: 'Too narrow', value: 1 },
+//     { name: 'Slightly narrow', value: 2 },
+//     { name: 'Perfect', value: 3 },
+//     { name: 'Slightly wide', value: 4 },
+//     { name: 'Too wide', value: 5 }
+// ];
+
+
+
---
+++ client/src/components/ratingandreviews/radioData/radioData.js
@@ -0,0 +1,58 @@
+const Radios = {
+
+    recommended: [
+        { name: 'Yes', value: true},
+        { name: 'No', value: false}
+    ],
+
+    size: [
+        { name: 'A size too small', value: 1 },
+        { name: '1/2 a size too small', value: 2 },
+        { name: 'Perfect', value: 3 },
+        { name: '1/2 a size too big', value: 4 },
+        { name: 'A size too big', value: 5 }
+    ],
+
+    width: [
+        { name: 'Too narrow', value: 1 },
+        { name: 'Slightly narrow', value: 2 },
+        { name: 'Perfect', value: 3 },
+        { name: 'Slightly wide', value: 4 },
+        { name: 'Too wide', value: 5 }
+    ],
+
+    comfort: [
+        { name: 'Uncomfortable', value: 1 },
+        { name: 'Slightly Uncomfortable', value: 2 },
+        { name: 'Ok', value: 3 },
+        { name: 'Comfortable', value: 4 },
+        { name: 'Perfect', value: 5 }
+    ],
+
+    quality: [
+        { name: 'Poor', value: 1 },
+        { name: 'Below Average', value: 2 },
+        { name: 'What I expected', value: 3 },
+        { name: 'Pretty great', value: 4 },
+        { name: 'Perfect', value: 5 }
+    ],
+
+    length: [
+        { name: 'Runs short', value: 1 },
+        { name: 'Runs slightly short', value: 2 },
+        { name: 'Perfect', value: 3 },
+        { name: 'Runs slightly long', value: 4 },
+        { name: 'Runs long', value: 5 }
+    ],
+
+    fit: [
+        { name: 'Runs tight', value: 1 },
+        { name: 'Runs slightly tight', value: 2 },
+        { name: 'Perfect', value: 3 },
+        { name: 'Runs slightly long', value: 4 },
+        { name: 'Runs long', value: 5 }
+    ]
+}
+
+export default Radios;
+
