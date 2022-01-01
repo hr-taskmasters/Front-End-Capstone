be11a0bc0813b44f68c73bdb8cca26877dbe4079
Ben Pintel <bpintel@gmail.com>
2022-01-01 12:12:50 -0800

    fixed another bug in rating breakdown when no ratings present for a givin number

--- client/src/components/ratingandreviews/Components/Ratings/RatingBreakdown.jsx
+++ client/src/components/ratingandreviews/Components/Ratings/RatingBreakdown.jsx
@@ -44,16 +44,16 @@ function RatingBreakdown (props) {
         <>
           <div>{percentRecommended}% of reviewers recommend this product</div> 
           <br></br>
-          <div>Five stars: {ratings['5']}</div>
-          <ProgressBar variant="success" now={ratings['5'] / totalRatings * 100} /> 
-          <div>Four stars: {ratings['4']}</div>
-          <ProgressBar variant="success" now={ratings['4'] / totalRatings * 100} /> 
-          <div>Three stars: {ratings['3']}</div>
-          <ProgressBar variant="success" now={ratings['3'] / totalRatings * 100} /> 
-          <div>Two stars: {ratings['2']}</div>
-          <ProgressBar variant="success" now={ratings['2'] / totalRatings * 100} /> 
-          <div>One star: {ratings['1']}</div>
-          <ProgressBar variant="success" now={ratings['1'] / totalRatings * 100} /> 
+          <div>Five stars: {ratings['5'] || '0'}</div>
+          <ProgressBar variant="success" now={(ratings['5'] || 0) / totalRatings * 100} /> 
+          <div>Four stars: {ratings['4'] || '0'}</div>
+          <ProgressBar variant="success" now={(ratings['4'] || 0) / totalRatings * 100} /> 
+          <div>Three stars: {ratings['3'] || '0'}</div>
+          <ProgressBar variant="success" now={(ratings['3'] || 0) / totalRatings * 100} /> 
+          <div>Two stars: {ratings['2'] || '0'}</div>
+          <ProgressBar variant="success" now={(ratings['2'] || 0) / totalRatings * 100} /> 
+          <div>One star: {ratings['1'] || '0'}</div>
+          <ProgressBar variant="success" now={(ratings['1'] || 0) / totalRatings * 100} /> 
           </>
           : <div className="spinner-border" role="status">
               <span className="visually-hidden">Loading...</span>
