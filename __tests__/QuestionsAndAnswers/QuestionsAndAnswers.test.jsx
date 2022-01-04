282d3ed80ec118414c1ca014b0e48c543c993bdc
“Cynthia <xin2604@gmail.com>
2022-01-04 10:19:50 -0800

    test existing of button and search bar

--- __tests__/QuestionsAndAnswers/QuestionsAndAnswers.test.jsx
+++ __tests__/QuestionsAndAnswers/QuestionsAndAnswers.test.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Enzyme, {shallow, mount} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import QuestionsAndAnswers from '../../client/src/components/questionsandanswers/QuestionsAndAnswers.jsx';
-import IndividualQuestion from '../../client/src/components/questionsandanswers/IndividualQuestion.jsx';
+import Search from '../../client/src/components/questionsandanswers/Search.jsx';
 Enzyme.configure({ adapter: new Adapter()});
 
 const product = {
@@ -16,12 +16,27 @@ const product = {
   default_price: "140.00",
   description: "The So Fatigues will wake you up and fit you in. This high energy camo will have you blending in to even the wildest surroundings.",
  };
-describe('Test Load More Questions Button component', () => {
-  it('Test click event', () => {
-    const mockCallBack = jest.fn();
-    const wrapper = shallow(<QuestionsAndAnswers product={product}/>);
-    wrapper.find('#loadBtn').simulate('click');
-    //expect(wrapper.find(IndividualQuestion)).toHaveLength(6);
-    expect(mockCallBack.mock.calls.length).toEqual(1);
+describe('Load More Questions Button component', () => {
+  it('Should be defined', () => {
+    //const mockCallBack = jest.fn();
+    // const wrapper = shallow(<QuestionsAndAnswers product={product}/>);
+    // wrapper.find('#loadBtn').simulate('click');
+    // //expect(wrapper.find(IndividualQuestion)).toHaveLength(6);
+    // expect(mockCallBack.mock.calls.length).toEqual(1);
+    const wrapper = mount(<QuestionsAndAnswers product={product}/>);
+    const loadMoreBtn = wrapper.find('#loadBtn')
+    expect(loadMoreBtn).toBeDefined();
+  });
+  it('Should render correctly', () => {
+    const wrapper = mount(<QuestionsAndAnswers product={product}/>);
+    expect(wrapper.find('#loadBtn')).toMatchSnapshot();
   })
 });
+
+describe('Search bar', () => {
+  it('Should render search bar', () => {
+    const wrapper = mount(<QuestionsAndAnswers product={product}/>);
+    expect(wrapper).toContainMatchingElement('Search');
+  })
+})
+
