d798d0a0757d476a553fd2259eaa6bb3639e159a
“Cynthia <xin2604@gmail.com>
2022-01-06 12:38:10 -0800

    data cleanup

--- client/src/components/questionsandanswers/Questions.jsx
+++ client/src/components/questionsandanswers/Questions.jsx
@@ -4,9 +4,8 @@ import IndividualQuestion from './IndividualQuestion.jsx';
 const Questions = (props) => (
   <div className='q_questions_sessions'>
     {props.questions.map((question, index) =>
-      <IndividualQuestion question={question} key={index} product_name={props.product_name}/>
+      <IndividualQuestion question={question} key={index} product_name={props.product_name} />
     )}
-    {/* <br></br> */}
   </div>
 );
 
